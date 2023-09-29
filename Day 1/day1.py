ddf9ecf153ddf952b74a30069979d8d708d3c498
knr2003 <114396360+knr2003@users.noreply.github.com>
2023-09-29 09:34:16 +0530

    Add files via upload

---
+++ Day 1/day1.py
@@ -0,0 +1,67 @@
+'''total_amount=int(input())
+i=int(input())
+m5=int(input())
+m9=int(input())
+interest_upto_5months=(total_amount*4/12*i)/100
+interest_upto_9months=((total_amount-m5)*4/12*i)/100
+remaining_months=((total_amount-m5+m9)*4/12*i)/100
+print((total_amount-m5+m9)+interest_upto_5months+interest_upto_9months+remaining_months)'''
+
+'''a=0
+print(id(a))
+a=10
+print(id(a))'''
+
+#int is immutable
+
+'''struct
+{
+    double
+    char
+}
+
+8+1+7--->16 bytes
+
+
+struct{
+    int
+    double
+    char
+}
+
+4+4+8+1+7--->24 bytes'''
+
+
+
+'''for i in range(0,5):
+    print(i)
+print("final: ",i)'''
+
+'''
+for(i=1;i<n;i+2){
+statements
+}
+
+n/2
+f(n)=n/2
+degree of polynomial is n . So n/  anything is n so here also O(n)
+
+So irrespective of iteration time is gonna be the same O(n).
+'''
+
+
+'''n=input()
+n1=0
+for i in n:
+    n1=n1+int(i)
+print(n1)'''
+
+
+'''n=int(input())
+sum1=0
+while(n!=0):
+    rem=n%10
+    sum1=sum1+rem
+    n=n//10
+print(sum1)'''
+    
