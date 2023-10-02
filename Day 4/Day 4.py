3afbe0a3ad0c84fa32e9113ebc0b08f13397c298
knr2003 <114396360+knr2003@users.noreply.github.com>
2023-10-02 18:22:39 +0530

    Add files via upload

---
+++ Day 4/Day 4.py
@@ -0,0 +1,29 @@
+#x=967070125678125124301.
+#In python every datatype is a class, in the below 'x' is considered as an object.
+
+'''
+x=967070125678125124301   'x' is integer type of object.
+print(type(x))'''
+
+'''Datatypes in python:
+    1.Numeric(integer,complex,float)
+    2.Dictionary
+    3.Boolean
+    4.set
+    5.Sequence type'(strings,list,tuple)'''
+'''
+everything is an object in Python programming,
+data types are actually classes and variables are instances (object) of these classes.'''
+
+'''import numpy as np
+  
+# Creating array object
+arr = np.array( [[ 1, 2, 3],
+                 [ 4, 2, 5]] )
+print("No. of dimensions: ", arr.ndim)
+  
+# Printing shape of array
+print("Shape of array: ", arr.shape)'''
+
+
+
